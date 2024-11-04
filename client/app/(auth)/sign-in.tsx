8703ae56a94c908fc1115252053a552fff332a8b
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-11-04 21:36:57 +0600

    client: add base routes. add icons, fonts

---
+++ client/app/(auth)/sign-in.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const SignIn = () => {
+  return <View>Sign-in page</View>;
+};
+
+export default SignIn;
--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,19 +1,18 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SF_PRO_BOLD: require("../assets/fonts/SFProDisplay-Bold.ttf"),
+    SF_PRO_REGULAR: require("../assets/fonts/SFProDisplay-Regular.ttf"),
+    SF_PRO_ITALIC: require("../assets/fonts/SFProDisplay-RegularItalic.ttf"),
+    SF_PRO_MEDIUM: require("../assets/fonts/SFProDisplay-Medium.ttf"),
   });
 
   useEffect(() => {
@@ -27,11 +26,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 }
